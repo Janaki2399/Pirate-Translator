0e485af6b9aa948235b6853c1edd64d4eb543093
Janaki <janakishreeja23@gmail.com>
2021-02-22 16:11:00 +0530

    refactor

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-var translateBtn=document.querySelector("#btn-translate");
-var input=document.querySelector(".in-textarea");
-var output=document.querySelector(".out-textarea");
+const translateBtn=document.querySelector("#btn-translate");
+const input=document.querySelector(".in-textarea");
+const output=document.querySelector(".out-textarea");
 
 translateBtn.addEventListener('click',function onClick(){
-    var inputText=input.value;
-    var url="https://api.funtranslations.com/translate/pirate.json"+"?"+"text="+inputText;
+    const inputText=input.value;
+    const url="https://api.funtranslations.com/translate/pirate.json"+"?"+"text="+inputText;
      fetch(url)
     .then(response => response.json())
     .then(json => output.innerText=json.contents.translated)
     .catch(errorHandler)
 });
-function errorHandler(error)
-{
-    alert("Some error occured .Please try again later");
-}
+const errorHandler=error=>alert("Some error occured .Please try again later");
